80d653af18df8be877a90b80cc0c24ad5d3b7817
danaasamuel2023 <157542114+danaasamuel2023@users.noreply.github.com>
2025-03-11 05:04:11 -1200

    hello

--- Client/next.config.mjs
+++ Client/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'standalone', // Ensure compatibility with Render
+    reactStrictMode: true,
+    trailingSlash: false,
+  };
+  
+  export default nextConfig;
+  
