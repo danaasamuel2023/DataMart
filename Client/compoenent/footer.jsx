d79e6fb73c6b17127ec64b67b49fd91e9bf6ab88
danaasamuel2023 <157542114+danaasamuel2023@users.noreply.github.com>
2025-03-17 12:37:50 -1200

    email at the footer changes

--- Client/compoenent/footer.jsx
+++ Client/compoenent/footer.jsx
@@ -80,7 +80,7 @@ const Footer = () => {
                 <svg className="h-5 w-5 mr-2 mt-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
                 </svg>
-                <span>info@datamartgh.com</span>
+                <span>unimarketgh@gmail.com</span>
               </p>
               <p className="flex items-start">
                 <svg className="h-5 w-5 mr-2 mt-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
