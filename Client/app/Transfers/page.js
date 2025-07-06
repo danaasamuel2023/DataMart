9bd401941c8cc829c4aa0c0732eb46e083c9b4ef
danaasamuel2023 <danaasamuel20frimpong@gmail.com>
2025-07-06 02:36:47 -1200

    use client

--- Client/app/Transfers/page.js
+++ Client/app/Transfers/page.js
@@ -1,3 +1,4 @@
+'use client';
 import React, { useState, useEffect } from 'react';
 import { Calendar, Search, TrendingUp, Users, DollarSign, AlertTriangle, Download, RefreshCw } from 'lucide-react';
 
