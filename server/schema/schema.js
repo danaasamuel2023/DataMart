4c7069fa6b647682226b06494420a3d0c870a11a
danaasamuel2023 <danaasamuel20frimpong@gmail.com>
2025-07-06 20:06:30 -1200

    worker

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -27,7 +27,7 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: function() { return !this.googleId; } }, // Not required for Google users
   phoneNumber: { type: String, required: true, unique: true },
-  role: { type: String, enum: ["buyer", "seller", "reporter", "admin", "Dealer"], default: "buyer" },
+  role: { type: String, enum: ["buyer", "seller", "reporter", "admin", "Dealer","worker"], default: "buyer" },
   walletBalance: { type: Number, default: 0 },
   referralCode: { type: String, unique: true },
   referredBy: { type: String, default: null },
